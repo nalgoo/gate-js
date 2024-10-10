db82e6b06dcd702959b488f0cb50abb82d0bbd13
Vlado Senasi <vladimir.senasi@nalgoo.com>
2024-10-10 11:07:55 +0200

    added ErrorBoundary around ApplyButton

--- packages/@gate-js-react/src/index.ts
+++ packages/@gate-js-react/src/index.ts
@@ -1,7 +1,8 @@
 export type * from '@gate-js/core';
 export type * from './types/types';
 
-export * from './components/apply-button/apply-button';
+export { ApplyButton as ApplyButtonRaw } from './components/apply-button/apply-button';
+export { ApplyButtonErrorBoundary as ApplyButton } from './components/apply-button/apply-button-error-boundary';
 export * from './components/gate-options/gate-options';
 export * from './components/job-content/job-content';
 export * from './components/job-details/job-details';
