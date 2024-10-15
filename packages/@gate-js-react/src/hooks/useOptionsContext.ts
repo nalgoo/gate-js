c3acd35b74adaf7d3d76f7ed83784885236cc683
Vlado Senasi <vladimir.senasi@nalgoo.com>
2024-10-15 10:39:14 +0200

    fixed re-render loop

--- packages/@gate-js-react/src/hooks/useOptionsContext.ts
+++ packages/@gate-js-react/src/hooks/useOptionsContext.ts
@@ -1,9 +1,11 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { OptionsType } from '@gate-js/core';
 import { OptionsContext } from '../context/options-context';
 
 export function useOptionsContext(overrideOptions: Partial<OptionsType> = {}): Partial<OptionsType> | null {
-	const ctx = useContext(OptionsContext) || {};
+	const ctx = useContext(OptionsContext);
 
-	return { ...ctx, ...overrideOptions };
+	const memoizedCtx = useMemo(() => ({ ...(ctx || {}), ...overrideOptions }), [ctx, overrideOptions]);
+
+	return memoizedCtx;
 }
