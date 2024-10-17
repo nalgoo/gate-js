ab14d7b5fee58279723b74133691c375668a1925
Vlado Senasi <vladimir.senasi@nalgoo.com>
2024-10-17 09:53:31 +0200

    removed prop from playground

--- playgrounds/react/src/app/page.tsx
+++ playgrounds/react/src/app/page.tsx
@@ -33,10 +33,11 @@ export default function Home() {
 						Daj nam o sebe vediet
 					</p>
 					<ApplyButton
-						className="rounded-full bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white
-									shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
-									focus-visible:outline-offset-2 focus-visible:outline-indigo-600
-								"
+						className="
+							rounded-full bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white
+							shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
+							focus-visible:outline-offset-2 focus-visible:outline-indigo-600
+						"
 						global
 						options={{ ...options, darkTheme: true }}
 					>
