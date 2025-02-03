13069ad80b31583ff337eaafc739531d6c2ec648
Vlado Senasi <vladimir.senasi@nalgoo.com>
2025-02-03 09:05:08 +0100

    added czech language

--- packages/@gate-js-react/src/localization/translations.ts
+++ packages/@gate-js-react/src/localization/translations.ts
@@ -1,4 +1,5 @@
 import { messages as sk } from './translations/sk';
 import { messages as en } from './translations/en';
+import { messages as cs } from './translations/cs';
 
-export const translations = { sk, en };
+export const translations = { sk, en, cs };
