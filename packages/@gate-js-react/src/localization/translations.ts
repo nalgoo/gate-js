bdb254984d02b5f7c50c8c1068bae581e516ff7e
Vlado Senasi <vladimir.senasi@nalgoo.com>
2025-02-12 11:15:19 +0100

    added hu translation

--- packages/@gate-js-react/src/localization/translations.ts
+++ packages/@gate-js-react/src/localization/translations.ts
@@ -1,5 +1,8 @@
 import { messages as sk } from './translations/sk';
 import { messages as en } from './translations/en';
 import { messages as cs } from './translations/cs';
+import { messages as hu } from './translations/hu';
 
-export const translations = { sk, en, cs };
+export const translations = {
+	sk, en, cs, hu,
+};
